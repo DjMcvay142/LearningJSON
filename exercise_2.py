15ac49edf8b30f7c809f2fb27b0c5d052b48fd54
Dan McVay <danielmcvaymedia@gmail.com>
2025-11-13 19:37:16 +0000

    Exercise 2 Complete

---
+++ exercise_2.py
@@ -0,0 +1,25 @@
+import json
+
+user_data = {
+    "firstName": "Dan",
+    "lastName": "McVay",
+    "university": "Northumbria University",
+    "course": "Computing and Information Science"
+}
+
+
+with open("user.json", "w") as file:
+    json.dump(user_data, file)
+
+with open("user.json", "r") as file:
+    data = json.load(file)
+    print(data)
+    print(f"Their first name is {data["firstName"]}")
+    data["academicYear"] = "Foundation Year"
+
+with open("user.json", "w") as file:
+    json.dump(data, file)
+
+with open("user.json", "r") as file:
+    loaded_data = json.load(file)
+    print(loaded_data)
